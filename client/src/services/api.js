2d01642d84ac014d9be9c3420a74968512a399d9
UnplannedCoder <pawansa2006@gmail.com>
2026-08-30 23:04:06 +0530

    Adding Files

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,22 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api/v1",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 // Request interceptor to attach JWT token
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 export default api;
