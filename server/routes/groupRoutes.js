8315a5d95fa596ba0f7e80547a8d9dd0d4f88e8a
UnplannedCoder <pawansa2006@gmail.com>
2026-09-01 14:44:05 +0530

    Adding Files

--- server/routes/groupRoutes.js
+++ server/routes/groupRoutes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
   getGroups,
@@ -11,17 +11,17 @@ const {
   addExpense,
   deleteExpense,
   settleMember,
-} = require('../controllers/groupController');
-const { protect } = require('../middleware/authMiddleware');
+} = require("../controllers/groupController");
+const { protect } = require("../middleware/authMiddleware");
 
 router.use(protect); // all group routes require authentication
 
-router.route('/').get(getGroups).post(createGroup);
-router.route('/:id').get(getGroupById).put(updateGroup).delete(deleteGroup);
-router.route('/:id/members').post(addMember);
-router.route('/:id/members/:memberId').delete(removeMember);
-router.route('/:id/members/:memberId/settle').put(settleMember);
-router.route('/:id/expenses').post(addExpense);
-router.route('/:id/expenses/:expenseId').delete(deleteExpense);
+router.route("/").get(getGroups).post(createGroup);
+router.route("/:id").get(getGroupById).put(updateGroup).delete(deleteGroup);
+router.route("/:id/members").post(addMember);
+router.route("/:id/members/:memberId").delete(removeMember);
+router.route("/:id/members/:memberId/settle").put(settleMember);
+router.route("/:id/expenses").post(addExpense);
+router.route("/:id/expenses/:expenseId").delete(deleteExpense);
 
 module.exports = router;
